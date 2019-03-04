ceaa41fc2a0c2c43f9bb4f40674e903f752ebc5d
MolnarArthur <arthur.molnar@usource.ro>
2019-03-04 10:25:21 +0200

    Ouput Incoming Request

--- hello.js
+++ hello.js
@@ -4,6 +4,7 @@ http.createServer(function (req, res) {
     fs.readFile(filename, function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
+        console.log("...Incoming Request: " + req.url);
         return res.end();
     });
 }).listen(8080);
