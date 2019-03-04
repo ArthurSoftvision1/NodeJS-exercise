fa6de7278123446346c317b935b821d048f4fa00
MolnarArthur <arthur.molnar@usource.ro>
2019-03-04 10:26:14 +0200

    Handle error if exists

--- hello.js
+++ hello.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+  
     fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         console.log("...Incoming Request: " + req.url);
