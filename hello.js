c3faa7f8e8e24311dcd604ac50b330804a0ebfc8
MolnarArthur <arthur.molnar@usource.ro>
2019-03-04 10:27:13 +0200

    Refactor filename

--- hello.js
+++ hello.js
@@ -1,7 +1,19 @@
 var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
 
 http.createServer(function (req, res) {
-  
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+
+    if (filename == './') {
+        filename = './index'
+    }
+
+    filename = filename + ".html";
+    console.log(filename);
+
     fs.readFile(filename, function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
