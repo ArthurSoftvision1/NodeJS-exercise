4e2dc352753304bf639cc26e15c583ba11463ab0
MolnarArthur <arthur.molnar@usource.ro>
2019-03-04 10:24:18 +0200

    Create Node development server

---
+++ hello.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    fs.readFile(filename, function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8080);
+
+console.log("Server Listening on Port 8080..");
